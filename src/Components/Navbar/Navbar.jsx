476b8617bd677a9ac9d165854af51d17975f39f9
Hamza Alvi <42583422+hamzaalvi1@users.noreply.github.com>
2020-07-31 00:26:06 +0500

    add components files and setupbasic routes

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import "./Navbar.css"
+import {Link} from 'react-router-dom'
+const useStyles = makeStyles(theme => ({
+    title: {
+      flexGrow: 2,
+      justifyContent: "space-between",
+      fontFamily: "Great Vibes",
+      fontSize: "32px",
+      cursor: "pointer"
+    },
+    main:{
+      backgroundColor: "#212121"
+    },
+    font:{
+     fontFamily: "Lato"
+    }
+  }));
+
+ const  Navbar = ()=> {
+   const classes = useStyles();
+
+  return (
+    <div>
+      <AppBar position="static" className = {classes.main}>
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Upper Fashions
+          </Typography>
+          <Link to = "/"> 
+          <Button color="inherit" className = {classes.font}>Home</Button>
+          </Link>
+          <Link to = "products"> 
+          <Button color="inherit" className = {classes.font}>Products</Button>
+          </Link>
+          <Link to = "signin"> 
+          <Button color="inherit" className = {classes.font}>Login</Button>
+          </Link>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default Navbar
---
+++ src/Components/RoutesConfig/RoutesConfig.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import Home from "../Home/Home"
+import Products from "../Products/Products"
+import Product from "../Product/Product"
+import SignIn from "../SignIn/SignIn"
+import SignUp from "../SignUp/SignUp"
+import NotFound from "../Notfound/NotFound"
+import Navbar from '../Navbar/Navbar'
+
+
+
+
+
+ const  RoutesConfig = ()=> {
+    return (
+        <div>
+            <Router>
+                <Navbar/>
+              <Routes>
+                  <Route path = "/" element = {<Home/>}/>
+                  <Route path = "products" element = {<Products/>}/>
+                  <Route path = "products:prodId" element = {<Product/>}/>
+                  <Route path = "signin" element = {<SignIn/>}/>
+                  <Route path = "signup" element = {<SignUp/>}/>
+                  <Route path = "*" element = {<NotFound/>}/>
+              </Routes>
+            </Router>
+        </div>
+    )
+}
+
+export default RoutesConfig
