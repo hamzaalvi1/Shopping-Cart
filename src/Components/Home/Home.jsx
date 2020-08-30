8144a46a858906c24260cea518740a43af0af0b4
Hamza Alvi <42583422+hamzaalvi1@users.noreply.github.com>
2020-08-30 18:22:54 +0500

    complete home page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import "./Home.css"
+import {useNavigate} from "react-router-dom"
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
  const  Home = ()=> {
+ const navigate = useNavigate()
     return (
-        <div>
-            <h1>Home Component</h1>
+        <div className = "home">
+            <div className = "main-text">
+            <Typography gutterBottom variant="h4" component="h2"  >
+             Upper Fashions
+             </Typography>
+             <Typography gutterBottom variant="h4" component="h3"  >
+             Finest Shoes Collection
+             </Typography>
+             <Button className = "main" color="primary" onClick = {()=> navigate("/products")}>
+              Go To Shop
+            </Button>
+       </div>
         </div>
     )
 }
